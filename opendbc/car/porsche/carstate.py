4d5ca9ce728fbf1e9c4241e27acbe30ff3b8b88b
ZwX1616 <zwx1616@gmail.com>
2026-02-15 22:50:05 -0800

    bus.cam

--- opendbc/car/porsche/carstate.py
+++ opendbc/car/porsche/carstate.py
@@ -3,14 +3,14 @@ from opendbc.can import CANDefine, CANParser
 from opendbc.car import Bus, structs
 from opendbc.car.common.conversions import Conversions as CV
 from opendbc.car.interfaces import CarStateBase
-from opendbc.car.porsche.values import DBC, CANBUS
+from opendbc.car.porsche.values import DBC
 
 class CarState(CarStateBase):
   def __init__(self, CP):
     super().__init__(CP)
 
   def update(self, can_parsers) -> structs.CarState:
-    cp = can_parsers[CANBUS]
+    cp = can_parsers[Bus.cam]
     ret = structs.CarState()
 
     ret.brake = cp.vl['PSM4']['PSM_BrakePressure'] # Bars
@@ -20,5 +20,5 @@ class CarState(CarStateBase):
   @staticmethod
   def get_can_parsers(CP):
     return {
-      CANBUS: CANParser(DBC[CP.carFingerprint][CANBUS], [], CANBUS),
+      Bus.cam: CANParser(DBC[CP.carFingerprint][Bus.cam], [], 1),
     }
