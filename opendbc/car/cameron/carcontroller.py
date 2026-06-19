cc5960a043a554885d2169ea4046684f73c7d5cc
ZwX1616 <zwx1616@gmail.com>
2026-06-18 21:06:47 -0700

    simple

--- opendbc/car/cameron/carcontroller.py
+++ opendbc/car/cameron/carcontroller.py
@@ -7,30 +7,23 @@ class CarController(CarControllerBase):
   def __init__(self, dbc_names, CP):
     super().__init__(dbc_names, CP)
     self.packer = CANPacker(dbc_names[Bus.pt])
-    self.last_tsr_ctr = -1
 
   def update(self, CC, CS, now_nanos):
     can_sends = []
 
-    if CS.cam_disp_values and CS.cam_tsr_values:
-      can_sends.append(disp_suppress(self.packer, CS.cam_disp_values))
+    if CS.cam_disp_values:
+      can_sends.append(cam_disp_suppress(self.packer, CS.cam_disp_values))
 
-      # FRONT_CAMERA_TSR (0xB5) carries a rolling code: byte1 low nibble is a
-      # counter (+8 mod 15) and b0/b6/b7 are a counter-keyed table. Re-emitting
-      # at our loop rate stalls the counter and faults the car, so forward only
-      # one edited copy per new camera frame (i.e. on counter change).
-      tsr_ctr = int(CS.cam_tsr_values["B1"]) & 0x0F
-      if tsr_ctr != self.last_tsr_ctr:
-        self.last_tsr_ctr = tsr_ctr
-        can_sends.append(tsr_suppress(self.packer, CS.cam_tsr_values))
+    if CS.cam_alert_values:
+      can_sends.append(cam_alert_suppress(self.packer, CS.cam_alert_values))
 
     self.frame += 1
     return CC.actuators, can_sends
 
 
-def disp_suppress(packer, cam_disp_values):
+def cam_disp_suppress(packer, cam_disp_values):
   disp_values = {sig: int(val) for sig, val in cam_disp_values.items()}
-  disp_values["SPEED_LIMIT"] = 145 # kph
+  disp_values["SPEED_LIMIT"] = 100
   disp_values["B1_LOW"] |= 0x06 # directionless ldw event? unclear
   disp_values["LL_CROSSING_LEFT"] = 0
   disp_values["LL_CROSSING_RIGHT"] = 0
@@ -42,10 +35,8 @@ def disp_suppress(packer, cam_disp_values):
   # disp_values["B6"] &= 0xFA
   return packer.make_can_msg("FRONT_CAMERA_DISP", 0, disp_values)
 
-
-def tsr_suppress(packer, cam_tsr_values):
-  # mirror the camera frame (preserves the rolling code b0/b1/b6/b7), overwrite
-  # only the displayed limit. emitted once per counter change by the caller.
-  tsr_values = {sig: int(val) for sig, val in cam_tsr_values.items()}
-  tsr_values["SPEED_LIMIT"] = 145 # kph
-  return packer.make_can_msg("FRONT_CAMERA_TSR", 0, tsr_values)
+def cam_alert_suppress(packer, cam_alert_values):
+  alert_values = {sig: int(val) for sig, val in cam_alert_values.items()}
+  alert_values["TSR_VISUAL"] = 0
+  alert_values["TSR_AUDIBLE"] = 0
+  return packer.make_can_msg("FRONT_CAMERA_ALERT", 0, alert_values)
--- opendbc/car/cameron/carstate.py
+++ opendbc/car/cameron/carstate.py
@@ -9,16 +9,14 @@ class CarState(CarStateBase):
     super().__init__(CP)
 
     self.cam_disp_values: dict = {}
-    self.cam_tsr_values: dict = {}
-    self.cam_ldw_values: dict = {}
+    self.cam_alert_values: dict = {}
 
   def update(self, can_parsers) -> structs.CarState:
     cp_cam = can_parsers[Bus.cam]
     ret = structs.CarState()
 
     self.cam_disp_values = cp_cam.vl["FRONT_CAMERA_DISP"]
-    self.cam_tsr_values = cp_cam.vl["FRONT_CAMERA_TSR"]
-    self.cam_ldw_values = cp_cam.vl["FRONT_CAMERA_LDW"]   # 0x210; parsed for observation only - NOT blocked/edited (B5-only test)
+    self.cam_alert_values = cp_cam.vl["FRONT_CAMERA_ALERT"]
 
     return ret
 
